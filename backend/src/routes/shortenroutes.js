6865634692431100b992b7b566a00461c2fafd5f
Rishi narkar 11 <rishinarkar16@gmail.com>
2026-02-10 15:53:06 +0530

    making frontend in react nad some changes in file structure

--- backend/src/routes/shortenroutes.js
+++ backend/src/routes/shortenroutes.js
@@ -6,16 +6,19 @@ router.post("/",async(req,res)=>{
     try{
         const{url}=req.body
         if(!url){return res.status(400).json({message:"Please provide a URL"})}
+        console.log("BODY:", req.body);
+
         const code =nanoid(6)
         await Url.create({
             originalUrl:url,
             shortCode:code
         })
+        return res.json({code})
       
     }
         catch(err){
             console.log(err)
-            res.status(500).json({message:"Server error"})
+            return res.json({code:null})
         }
 })
 export default router
